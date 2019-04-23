054fc7e206ac3f39c517f6ccd8934b7b014b45d4
mcmtrnt <mcmtrnt3@gmail.com>
2019-04-22 22:05:39 -0600

    commit

--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1555468263.5612998
+_modified_time = 1555992135.4728198
 _enable_loop = True
 _template_filename = 'C:/Users/Trent/dirtbikedeals/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -27,7 +27,7 @@ def render_body(context,**pageargs):
         __M_writer('        <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">\r\n        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>\r\n        \r\n        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\r\n\r\n')
         __M_writer('        <script src="/django_mako_plus/dmp-common.min.js"></script>\r\n        ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( django_mako_plus.links(self) ))
-        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>      \r\n                <div class="nav navbar-default custom-nav justify-content-between">    \r\n                        <h1>Dirt Bike Deals</h1>                \r\n                </div>  \r\n        </header>\r\n\r\n        <main>\r\n            ')
+        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <header>      \r\n                <div class="nav navbar-default custom-nav justify-content-between">    \r\n                        <h1>Dirt Bike Deals</h1>      \r\n                        \r\n                        <li class="nav-item active">\r\n                            <a class="btn btn-outline-primary" href="/homepage/deals/">Deals<span class="sr-only">(current)</span></a>\r\n                        </li>\r\n                       \r\n                  \r\n                        <li class="nav-item active">\r\n                            <a class="btn btn-outline-primary" href="/homepage/calculator/">Price my bike<span class="sr-only">(current)</span></a>\r\n                        </li>\r\n                </div>  \r\n        </header>\r\n\r\n        <main>\r\n            ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
@@ -52,6 +52,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Trent/dirtbikedeals/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "26": 2, "27": 10, "28": 18, "29": 19, "30": 19, "35": 33, "41": 31, "47": 31, "53": 47}}
+{"filename": "C:/Users/Trent/dirtbikedeals/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "26": 2, "27": 10, "28": 18, "29": 19, "30": 19, "35": 42, "41": 40, "47": 40, "53": 47}}
 __M_END_METADATA
 """
