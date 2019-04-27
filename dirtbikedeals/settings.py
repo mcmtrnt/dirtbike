d6f293b6a90ca885ba5fc4051d9358746b95fa25
mcmtrnt <mcmtrnt3@gmail.com>
2019-04-27 17:48:51 -0600

    finish

--- dirtbikedeals/settings.py
+++ dirtbikedeals/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # SECURITY WARNING: this next line must be commented out at deployment
-    BASE_DIR,
+    # BASE_DIR,
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
